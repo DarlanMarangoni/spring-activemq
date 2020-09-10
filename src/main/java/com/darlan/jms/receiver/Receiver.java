324c54393bdac221d4487ccd419e9705777d126b
Darlan <marangoni.darlan1@gmail.com>
2020-09-10 15:33:12 -0300

    Simplificando

--- src/main/java/com/darlan/jms/receiver/Receiver.java
+++ src/main/java/com/darlan/jms/receiver/Receiver.java
@@ -1,13 +1,12 @@
 package com.darlan.jms.receiver;
 
-import com.darlan.jms.model.Email;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
 
-    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+    @JmsListener(destination = "mailbox")
     public void receiveMessage(String email) {
         System.out.println("Received <" + email + ">");
     }
