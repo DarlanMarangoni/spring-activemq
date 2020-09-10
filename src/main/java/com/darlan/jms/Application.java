ac3a015d0005664997264d3bda02a78e8825e786
Darlan <marangoni.darlan1@gmail.com>
2020-09-10 15:28:53 -0300

    Consumindo Json

--- src/main/java/com/darlan/jms/Application.java
+++ src/main/java/com/darlan/jms/Application.java
@@ -30,23 +30,12 @@ public class Application {
         return factory;
     }
 
-//    @Bean // Serialize message content to json using TextMessage
-//    public MessageConverter jacksonJmsMessageConverter() {
-//        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-//        converter.setTargetType(MessageType.TEXT);
-//        converter.setTypeIdPropertyName("_type");
-//        return converter;
-//    }
-
     public static void main(String[] args) {
         // Launch the application
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 
         JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
 
-//        // Send a message with a POJO - the template reuse the message converter
-//        System.out.println("Sending an email message.");
-//        jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "Hello"));
     }
 
 }
