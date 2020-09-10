aded36107abfff3471832c9d47e1435eb2fea3f2
Darlan <marangoni.darlan1@gmail.com>
2020-09-10 14:11:30 -0300

    send and receive message

---
+++ src/main/java/com/darlan/jms/receiver/Receiver.java
@@ -0,0 +1,14 @@
+package com.darlan.jms.receiver;
+
+import com.darlan.jms.model.Email;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+    public void receiveMessage(Email email) {
+        System.out.println("Received <" + email + ">");
+    }
+}
